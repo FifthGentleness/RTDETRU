cc3b84f9bab8fc8f3333cad5fc5d39b0682d8005
FifthGentleness <1773615003@qq.com>
2026-08-30 18:50:56 +0800

    fixpatience

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
                 cache=False,
                 imgsz=640,
                 epochs=350,
-                patience=50,
+                patience=0,
                 batch=4,
                 workers=4,
                 pretrained=False,
